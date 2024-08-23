3f58e7e4d8460b81f9fdf946504901e922bc4b49
Melkamu <melkemech@gmail.com>
2024-08-23 06:03:35 +0300

    Add perimeter of a rectangle

--- rectangle.py
+++ rectangle.py
@@ -2,3 +2,7 @@ w = float(input ("inter the width of the rectangle: "))
 l = float(input ("inter the length of the rectangle: "))
 a = w * l
 print("area of the rectangle =", a )
+p = 2*l + 2*w
+print("perimeter of the rectangle =", p )
+
+
