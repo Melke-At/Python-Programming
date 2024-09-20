fbcced3cc3c40e2c1c1c3b6091ab590e8bd286c0
Melkamu <melkemech@gmail.com>
2024-09-20 07:24:43 +0300

    Module8 airport name added

--- Module7/exc3.py
+++ Module7/exc3.py
@@ -9,19 +9,37 @@ connection = mysql.connector.connect(
     autocommit = True
 
 )
-def get_airport_by_ident(ICAO):
-    sql = f"select*from airport where ident = '{ICAO}'"
+def get_airport_name(icao):
+    sql = f"select name from airport where ident='{icao}'"
 
     cursor = connection.cursor()
     cursor.execute(sql)
-    result = cursor.fetchall()
+    result = cursor.fetchone()
     return result
-code = input('type ICAO code: ')
-airport = get_airport_by_ident(code)
-if airport is not None:
-    print(f'{airport[0]} is located in {airport[0]} in {airport[0]}')
-else:
-    print('airport not found')
+
+
+def display_menu(choice):
+    print ("Enter your choice")
+    choice = [1, 2, 3]
+
+    for i in range(len(choice)):
+       print ({choice[i]})
+
+def get_user_choice():
+    while True:
+        display_menu(1)
+        choice = input("Enter the number of your choice: ")
+        if choice == 1:
+            newcode = input(f'Enter new airport icao code: ')
+            newname = input(f'Enter new airport name: ')
+            print (f"you have entered new airport with the code {newcode} and name{newname} ")
+        elif choice == 2:
+            airport_code = input(f"Do you want to fetch from the existing airport ")
+            get_airport_name(airport_code)
+        elif choice == 3:
+            break
+
+get_user_choice()
 
 
 
--- Module8/exc3.py
+++ Module8/exc3.py
@@ -10,10 +10,16 @@ connection = mysql.connector.connect(
     password = 'password',
     autocommit = True
 )
+def get_airport_name (ICAO):
+    sql = f"select name from airport where ident = '{ICAO}'"
+    cursor = connection.cursor()
+    cursor.execute(sql)
+    airport_name = cursor.fetchone()
+    return airport_name
 
 def get_location_by_ICAO (ICAO):
     sql = f"select latitude_deg, longitude_deg from airport where ident = '{ICAO}'"
-    print(sql)
+    #print(sql)
     cursor = connection.cursor()
     cursor.execute(sql)
     result = cursor.fetchone()
@@ -23,16 +29,16 @@ def get_location_by_ICAO (ICAO):
         print(f"No airport found with ICAO code: {ICAO}")
         return None
 def calculate_distance_between_airport(code1,code2):
-    code1 = get_location_by_ICAO(code1)
-    code2 = get_location_by_ICAO(code2)
+    cor1 = get_location_by_ICAO(code1)
+    cor2 = get_location_by_ICAO(code2)
     if code1 and code2:
-        distance = geodesic(code1, code2).kilometers
-        print(f"The distance between {code1} and {code2} is {distance} km")
+        distance = geodesic(cor1, cor2).kilometers
+        print(f"The distance between {get_airport_name(code1)} and {get_airport_name(code2)} is {distance} km")
 
 
     else:
         print("unable to print the distance due to the missing data")
-code1 = input("Enter the first ICAO code: ")
-code2 = input("Enter the second ICAO code: ")
-calculate_distance_between_airport(code1, code2)
+loc1 = input("Enter the first ICAO code: ")
+loc2 = input("Enter the second ICAO code: ")
+calculate_distance_between_airport(loc1, loc2)
 connection.close()
