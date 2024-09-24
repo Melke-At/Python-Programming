4f726a95ddf743de6edb04b61494b06e81744af5
Melkamu <melkemech@gmail.com>
2024-09-24 17:05:42 +0300

    exc3

--- Module7/exc3.py
+++ Module7/exc3.py
@@ -1,46 +1,61 @@
 import mysql.connector
 
 connection = mysql.connector.connect(
-    host = '127.0.0.1',
-    port = 3306,
-    database = 'flight_game',
-    user = 'root',
-    password = 'password',
-    autocommit = True
-
+    host='127.0.0.1',
+    port=3306,
+    database='flight_game',
+    user='root',
+    password='password',
+    autocommit=True
 )
-def get_airport_name(icao):
-    sql = f"select name from airport where ident='{icao}'"
 
+def insert_new_airport(airport_name, airport_icao):
+    sql = "INSERT INTO airport (name, ident) VALUES (%s, %s)"
     cursor = connection.cursor()
-    cursor.execute(sql)
-    result = cursor.fetchone()
-    return result
+    cursor.execute(sql, (airport_name, airport_icao))
+    print(f"A new airport {airport_name} with ICAO code {airport_icao} has been added.")
+    return
 
+def get_airport_name(icao_code):
+    sql = f"select name from airport where ident = '{icao_code}'"
+    cursor = connection.cursor()
+    cursor.execute(sql, (icao_code))
+    airport_name = cursor.fetchone()
+    if airport_name:
+        return airport_name[0]
+    else:
+        return None
 
-def display_menu(choice):
-    print ("Enter your choice")
-    choice = [1, 2, 3]
+def register_airport_name():
+    new_airport = input("Enter the name of new airport: ")
+    icao_code = input("Enter ICAO code: ")
 
-    for i in range(len(choice)):
-       print ({choice[i]})
+    if get_airport_name(icao_code):
+        print(f"The ICAO code {icao_code} already exists in the database.")
+    else:
+        insert_new_airport(new_airport, icao_code)
 
-def get_user_choice():
-    while True:
-        display_menu(1)
-        choice = input("Enter the number of your choice: ")
-        if choice == 1:
-            newcode = input(f'Enter new airport icao code: ')
-            newname = input(f'Enter new airport name: ')
-            print (f"you have entered new airport with the code {newcode} and name{newname} ")
-        elif choice == 2:
-            airport_code = input(f"Do you want to fetch from the existing airport ")
-            get_airport_name(airport_code)
-        elif choice == 3:
-            break
+def airport_information():
+    icao_code = input("Enter ICAO code: ")
+    airport_name = get_airport_name(icao_code)
+    if airport_name:
+        print(f"The name of the airport with ICAO code {icao_code} is: {airport_name}")
+    else:
+        print(f"There is no information available for ICAO code {icao_code}")
 
-get_user_choice()
+while True:
+    print("\nAirport Data Program")
+    print("A. Add a new airport")
+    print("B. Fetch airport information")
+    print("C. Quit")
 
-
-
-
+    choice = input("Enter your choice (A/B/C): ").upper()
+    if choice == "A":
+        register_airport_name()
+    elif choice == "B":
+        airport_information()
+    elif choice == "C":
+        print("Exiting the program.")
+        break
+    else:
+        print("Invalid choice, please select A, B, or C.")
